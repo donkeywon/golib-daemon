1146d943454a2d4bdd02e75c67cf26ff850930c9
donkeywon <donkeywon@163.com>
2024-08-02 17:50:23 +0800

    add profd flag description

--- profd/cfg.go
+++ profd/cfg.go
@@ -12,17 +12,17 @@ const (
 )
 
 type Cfg struct {
-	EnableStartupProfiling bool   `yaml:"enableStartupProfiling"   env:"PROF_ENABLE_STARTUP_PROFILING"   flag-long:"prof-enable-startup-profiling"`
-	StartupProfilingSec    int    `yaml:"startupProfilingSec"      env:"PROF_STARTUP_PROFILING_SEC"      flag-long:"prof-startup-profiling-sec"`
-	StartupProfilingMode   string `yaml:"startupProfilingMode"     env:"PROF_STARTUP_PROFILING_MODE"     flag-long:"prof-startup-profiling-mode"`
-	ProfilingOutputDir     string `yaml:"profilingOutputDir"       env:"PROF_OUTPUT_DIR"                 flag-long:"prof-output-dir"`
+	EnableStartupProfiling bool   `yaml:"enableStartupProfiling"   env:"PROF_ENABLE_STARTUP_PROFILING"   flag-long:"prof-enable-startup-profiling" flag-description:"profiling at startup"`
+	StartupProfilingSec    int    `yaml:"startupProfilingSec"      env:"PROF_STARTUP_PROFILING_SEC"      flag-long:"prof-startup-profiling-sec"    flag-description:"startup profiling duration in seconds, only works when prof-enable-startup-profiling is enabled"`
+	StartupProfilingMode   string `yaml:"startupProfilingMode"     env:"PROF_STARTUP_PROFILING_MODE"     flag-long:"prof-startup-profiling-mode"   flag-description:"startup profiling mode, only works when prof-enable-startup-profiling is enabled"`
+	ProfilingOutputDir     string `yaml:"profilingOutputDir"       env:"PROF_OUTPUT_DIR"                 flag-long:"prof-output-dir"               flag-description:"dir path of pprof file save to"`
 
-	EnableGoPs bool   `yaml:"enableGoPs"               env:"PROF_ENABLE_GOPS"                flag-long:"prof-enable-gops"`
-	GoPsAddr   string `yaml:"goPsAddr"                 env:"PROF_GOPS_ADDR"                  flag-long:"prof-gops-addr"`
+	EnableHTTPPprof bool `yaml:"enableHTTPPprof" env:"PROF_ENABLE_HTTP_PPROF" flag-long:"prof-enable-http-pprof" flag-description:"enable pprof over http, need httpd"`
 
-	EnableHTTPPprof bool `yaml:"enableHTTPPprof" env:"PROF_ENABLE_HTTP_PPROF" flag-long:"prof-enable-http-pprof"`
+	EnableGoPs bool   `yaml:"enableGoPs" env:"PROF_ENABLE_GOPS" flag-long:"prof-enable-gops" flag-description:"enable gops agent"`
+	GoPsAddr   string `yaml:"goPsAddr"   env:"PROF_GOPS_ADDR"   flag-long:"prof-gops-addr"   flag-description:"gops agent listen addr"`
 
-	EnableStatsViz bool `yaml:"enableStatsViz" env:"PROF_ENABLE_STATS_VIZ" flag-long:"prof-enable-stats-viz"`
+	EnableStatsViz bool `yaml:"enableStatsViz" env:"PROF_ENABLE_STATS_VIZ" flag-long:"prof-enable-stats-viz" flag-description:"enable statsviz, need httpd"`
 }
 
 func NewCfg() *Cfg {
